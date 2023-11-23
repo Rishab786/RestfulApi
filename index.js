a8b1f296e98ce3ad130954bfb91211a959c98048
rishabsingla5@gmail.com <rishabsingla5@gmail.com>
2023-11-23 12:38:29 +0530

    finalCommit

--- index.js
+++ index.js
@@ -19,40 +19,16 @@ function AddProduct(e) {
     };
     axios
       .post(
-        "https://crudcrud.com/api/7220c17e9d0141679c48a389b8d9061c/Admin",
+        "https://crudcrud.com/api/2dea1fdbe45145a9b2f0d47d174f42a3/Admin",
         obj
       )
+      .then(function (response) {
+        createElement(response.data);
+      })
+
       .catch(function (error) {
         console.log(error);
       });
-      let list;
-      if(category=='Electronic')
-      {
-        list= document.getElementById("electronic");
-         }
-      else if(category=='Food')
-      {
-        list= document.getElementById("food");
-      }
-      else{
-        list= document.getElementById("skincare");
-      }
-     
-    const item = document.createElement("li");
-    item.className = "";
-    item.innerText = amount + " " + description + " " + category + "  ";
-    const deleteBtn = document.createElement("button");
-    
-    deleteBtn.textContent = "Delete";
-    
-    item.appendChild(deleteBtn);
-    
-    list.appendChild(item);
-    deleteBtn.onclick = () => {
-      deleteData(item.id);
-    };
-
-    clear();
   }
 }
 function clear() {
@@ -60,56 +36,57 @@ function clear() {
   document.getElementById("desc").value = "";
   document.getElementById("category").value = "Select Category";
 }
+
 window.addEventListener("DOMContentLoaded", display);
+
 function display() {
   axios
-    .get(
-      "https://crudcrud.com/api/7220c17e9d0141679c48a389b8d9061c/Admin"
-    )
+    .get("https://crudcrud.com/api/2dea1fdbe45145a9b2f0d47d174f42a3/Admin")
     .then(function (response) {
       const productData = response.data;
       for (let i = 0; i < productData.length; i++) {
-        const amount = productData[i].Amount;
-        const description = productData[i].Description;
-        const category = productData[i].Category;
-        let list;
-        if(category=='Electronic')
-        {
-          list= document.getElementById("electronic");
-           }
-        else if(category=='Food')
-        {
-          list= document.getElementById("food");
-        }
-        else{
-          list= document.getElementById("skincare");
-        }
-        const item = document.createElement("li");
-        item.id = productData[i]._id;
-        item.className = "";
-        item.innerText = amount + " " + description + " " + category + "  ";
-        const deleteBtn = document.createElement("button");
-        
-        deleteBtn.textContent = "Delete";
-        item.appendChild(deleteBtn);
-        list.appendChild(item);
-        deleteBtn.onclick = () => {
-          deleteData(item.id);
-        };
-       
+        createElement(productData[i]);
       }
     });
 }
 function deleteData(id) {
-  const URL = `https://crudcrud.com/api/7220c17e9d0141679c48a389b8d9061c/Admin/${id}`;
-
   axios
-    .delete(URL)
-    .then(function () {
-      console.log("deleted Successfully");
+    .delete(
+      `https://crudcrud.com/api/2dea1fdbe45145a9b2f0d47d174f42a3/Admin/${id}`
+    )
+    .then(function (response) {
+      console.log(response);
+      console.log(id);
     })
     .catch(function (err) {
       console.log(err);
     });
 }
+function createElement(user) {
+  let list;
+  const amount = user.Amount;
+  const description = user.Description;
+  const category = user.Category;
+  if (category == "Electronic") {
+    list = document.getElementById("electronic");
+  } else if (category == "Food") {
+    list = document.getElementById("food");
+  } else {
+    list = document.getElementById("skincare");
+  }
 
+  const item = document.createElement("li");
+  item.id = user._id;
+  item.innerText = amount + " " + description + " " + category + "  ";
+  const deleteBtn = document.createElement("button");
+
+  deleteBtn.textContent = "Delete";
+
+  item.appendChild(deleteBtn);
+
+  list.appendChild(item);
+  //deleteBtn.setAttribute("onclick",deleteData(user._id));
+  deleteBtn.onclick = () => {
+    deleteData(user._id);
+  };
+}
